7bfb30e9feb221b3e9bd2aed0f042fdb440f967c
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2015-07-26 22:03:04 +0200

    Little modification of the template

--- src/main/scala/fr/winbee/service/OdsBuilderService.scala
+++ src/main/scala/fr/winbee/service/OdsBuilderService.scala
@@ -8,7 +8,7 @@ import org.jopendocument.dom.spreadsheet.SpreadSheet
 
 class OdsBuilderService {
 
-  def create(recordList: List[Record], templateFile : File,outputFile: File): Unit = {
+  def create(recordList: List[Record], templateFile: File, outputFile: File): Unit = {
     // Load the file.
     val spreadSheet = SpreadSheet.createFromFile(templateFile)
     val sheet = spreadSheet.getFirstSheet
@@ -20,7 +20,7 @@ class OdsBuilderService {
       mutableTableModel.setValueAt(record.date, rowIndex, 0)
       mutableTableModel.setValueAt(record.description, rowIndex, 1)
       mutableTableModel.setValueAt(record.name, rowIndex, 2)
-      mutableTableModel.setValueAt(record.value, rowIndex, 3)
+      mutableTableModel.setValueAt(record.value.toDouble / 100, rowIndex, 3)
       rowIndex += 1
     }
 
